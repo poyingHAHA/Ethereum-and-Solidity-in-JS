d4a42c8a3b0a76089540931a451c03392aea570e
poyingHAHA <poying.code@gmail.com>
2022-11-11 20:38:48 +0800

    fetching accounts from Ganache(local testing betwork)

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,29 +3,16 @@ const ganache = require("ganache-cli"); //this is going to serve as our local Et
 const Web3 = require("web3"); //Whenever we make use of web three, we are always going to be requiring in or importing a constructor function
 const web3 = new Web3(ganache.provider()); //here is what creates an instance of web three and tells that instance to attempt to connect to this local test network
 
-class Car {
-  park() {
-    return "stopped";
-  }
-
-  drive() {
-    return "vroom";
-  }
-}
-
-let car;
-beforeEach(() => {
-    car = new Car();
-}) // code inside will be called before each it function
-
-describe("Car Class", () => {
-  it("can park", () => {
-    // const car = new Car();
-    assert.equal(car.park(), "stopped");
-  });
-
-  it('can drive', () => {
-    // const car = new Car();
-    assert.equal(car.drive(), "vroom");
-  })
+let accounts;
+before(async () => {
+    // Get a list of all accounts
+    // Every function we call with web3 is asynchronous in nature which means it's going to return Promise
+    accounts = await web3.eth.getAccounts()
+    // Use one of those accounts to deploy the contract
 });
+
+describe("Inbox", () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    });
+})
